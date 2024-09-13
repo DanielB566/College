48cbe6aba8a9eeaa5c576e87a4d3c0f799faf9a1
DanielB566 <Danib7382@gmail.com>
2024-09-13 18:27:41 -0400

    Made Page 2 responsive on all screens

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-
-//https://www.winterwind.com/tutorials/css/25 Credits//
-
 window.onscroll = function() { changeImage() }
+      
+function changeImage() {
+  var scroll = window.scrollY + (window.innerHeight/3);
 
-    function changeImage() {
-      var scroll = window.scrollY + (window.innerHeight/3);
+  [...document.getElementsByClassName('section')].forEach(el => {
+    el.classList.remove('active');
 
-      [...document.getElementsByClassName('section')].forEach(el => {
-        el.classList.remove('active');
+    if(el.offsetTop <= scroll && el.offsetTop + el.offsetHeight > scroll) {
+      el.classList.add('active');
+    }
+  })
+} changeImage();
 
-        if(el.offsetTop <= scroll && el.offsetTop + el.offsetHeight > scroll) {
-          el.classList.add('active');
-        }
-      })
-    } changeImage();
+
+// Credits: https://www.winterwind.com/tutorials/css/25 // 
